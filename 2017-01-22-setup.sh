4b9c2bafa0712545c48c5054b08460f689708e8d
Ubuntu <ubuntu@ip-10-0-0-9.eu-west-1.compute.internal>
2017-01-22 09:29:51 +0000

    First commit after setup on t2.large

---
+++ 2017-01-22-setup.sh
@@ -0,0 +1,97 @@
+sudo
+vim setup.sh
+bash setup.sh 
+fg
+vim setup.sh 
+echo $path
+echo $PATH
+fg
+ls ~/.keras/
+fg
+vim .theanorc 
+fg
+bash setup.sh 
+man conda
+conda --help
+fg
+source .bashrc
+echo $PATH
+tail .bashrc
+conda --help
+fg
+pip install -y
+man pip
+pip --help
+fg
+bash setup.sh 
+fg
+rm downloads/*
+ls
+rm setup.sh 
+rm .ssh/authorized_keys 
+locale
+ls
+ls nbs
+ls downloads/
+which python
+python --version
+jupyter notebook
+cd nbs/
+jupyter notebook
+ls
+ls downloads/
+ls anaconda2/
+ls nbs/
+sudo apt-get install unzip
+cd nbs/
+wget http://www.platform.ai/files/nbs/lesson1.ipynb
+ls
+wget http://www.platform.ai/files/nbs/utils.zip
+wget http://www.platform.ai/files/nbs/vgg16.zip
+ls
+unzip utils.zip 
+unzip vgg16.zip 
+ls
+tree 
+sudo apt-get install tree
+mkdir data
+cd data
+wget http://www.platform.ai/files/dogscats.zip
+unzip dogscats.zip 
+tree -d
+sudo apt-get install tree
+sudo apt-get update
+tree
+sudo apt-get update
+sudo apt-get install tree
+tree -d
+ls dogscats/test1
+cd ..
+git clone https://github.com/fastai/courses.git
+sudo apt-get install git
+jupyter notebook
+host
+hosts
+cat /etc/host
+cat /etc/hosts
+uname -a
+jupyter notebook
+bg
+pip install cv2
+pip install --upgrade pip
+pip install cv2
+ls
+cd nbs
+vim utils.
+vim utils.py
+pip install --upgrade keras
+bg
+ls
+ls -lah
+history
+history -h
+history --help
+man history
+hist
+history
+history >> 2017-01-22-setup.sh
