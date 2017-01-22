e0f66394aa10ee458d05115f1df18d7eb66f6532
mingrisch <michael.ingrisch@googlemail.com>
2017-01-22 20:25:53 +0000

    fix sample preparation

--- kaggle-cats-dogs/data/datapreparation.py
+++ kaggle-cats-dogs/data/datapreparation.py
@@ -10,23 +10,31 @@ random.shuffle(cats)
 random.shuffle(dogs
 )
 
-valcats = cats[25:2500]
-valdogs = dogs[25:2500]
+valcats = cats[50:2500]
+valdogs = dogs[50:2500]
 traincats = cats[2500:]
 traindogs = dogs[2500]
 traindogs = dogs[2500:]
-sampledogs = dogs[:25]
-samplecats = cats[:25]
+samplevaldogs = dogs[25:50]
+samplevalcats = cats[25:50]
+
+sampletraindogs = dogs[:25]
+sampletraincats = cats[:25]
 
 os.mkdir('train')
 os.mkdir('valid')
 os.mkdir('sample')
+os.mkdir('sample/train')
+os.mkdir('sample/valid')
+
 os.mkdir('train/cat')
 os.mkdir('train/dog')
 os.mkdir('valid/dog')
 os.mkdir('valid/cat')
-os.mkdir('sample/dog')
-os.mkdir('sample/cat')
+os.mkdir('sample/valid/dog')
+os.mkdir('sample/valid/cat')
+os.mkdir('sample/train/dog')
+os.mkdir('sample/train/cat')
 
 from os.path import join as opj
 import shutil
@@ -52,12 +60,22 @@ target = 'train/cat'
 for el in ll:
     shutil.move(opj('ttrain', el), target)
 
-ll = sampledogs
-target = 'sample/dog'
+ll = samplevaldogs
+target = 'sample/valid/dog'
 for el in ll:
     shutil.move(opj('ttrain', el), target)
 
-ll = samplecats
-target = 'sample/cat'
+ll = samplevalcats
+target = 'sample/valid/cat'
+for el in ll:
+    shutil.move(opj('ttrain', el), target)
+
+ll = sampletraindogs
+target = 'sample/train/dog'
+for el in ll:
+    shutil.move(opj('ttrain', el), target)
+
+ll = sampletraincats
+target = 'sample/train/cat'
 for el in ll:
     shutil.move(opj('ttrain', el), target)
